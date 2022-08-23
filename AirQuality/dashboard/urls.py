d529ef2abdf19f694e44f5dc7f41085edb7c66b9
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2022-08-23 17:03:18 +0600

    added form to input air quality data

--- AirQuality/dashboard/urls.py
+++ AirQuality/dashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('create_air_quality_data/',views.StationAirQualityCreate.as_view(),name='create_air_quality_data')
 ]
--- AirQuality/dashboard/views.py
+++ AirQuality/dashboard/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import CreateView
+from django import forms
+
+from . import models
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("hello")
+
+class StationAirQualityCreate(CreateView):
+    model = models.StationAirQuality
+    fields = '__all__'
+
+    def get_form(self,form_class=None):
+        form = super().get_form(form_class)
+        form.fields["Date"].widget = forms.DateInput(format=('%d/%m/%Y'),attrs={'class':'form_control','placeholder':'Select a date','type':'date'})
+        return form
+    
